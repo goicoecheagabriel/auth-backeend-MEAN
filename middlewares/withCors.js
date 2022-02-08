9d65b22dbdcffe37ddf6cecac24fc873b6ebac56
gabriel <goicoecheagabriel@gmail.com>
2022-02-08 20:15:36 +0100

    Borramos la aplicacione cliente para proximo reemplazo

--- middlewares/withCors.js
+++ middlewares/withCors.js
@@ -7,6 +7,7 @@ const withCors = ( req, res = response, next ) => {
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    console.log(":::DESDE:withCors",res.header);
     next();
 
 }
